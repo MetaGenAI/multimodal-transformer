(hopefully) some fixes to attention mask

models/transformer.py
@@ -54,7 +54,8 @@ class TransformerCausalModel(nn.Module):
         # src = self.encoder(src) * math.sqrt(self.dinp)
         src *= math.sqrt(self.dinp)
         src = self.pos_encoder(src)
-        src = self.encoder1(src)
+        # src = self.encoder1(src)
+        # print(src)
         output = self.transformer_encoder(src, src_mask)
         output = self.decoder(output)
         return output

models/transformer_model.py
@@ -131,7 +131,8 @@ class TransformerModel(BaseModel):
         input_time_offsets = self.input_time_offsets
         self.src_masks = []
         for i, mod in enumerate(input_mods):
-            self.src_masks.append(self.input_mod_nets[i].generate_square_subsequent_mask(input_lengths[i], input_lengths[i]-predicted_inputs[i]).to(self.device))
+            mask = self.input_mod_nets[i].generate_square_subsequent_mask(input_lengths[i], input_lengths[i]-predicted_inputs[i]).to(self.device)
+            self.src_masks.append(mask)
 
         input_present_matrix = torch.zeros(len(input_mods),max(np.array(input_lengths)+np.array(input_time_offsets)))
         for i, mod in enumerate(input_mods):
@@ -150,6 +151,11 @@ class TransformerModel(BaseModel):
         self.output_mask = self.output_mod_nets[0].generate_square_subsequent_mask(sum(input_lengths), 0)
         self.output_mask = self.output_mask[input_indices.unsqueeze(0).T,input_indices.unsqueeze(0)]
         print(self.output_mask.shape)
+        j=0
+        for i, mod in enumerate(input_mods):
+            # self.output_mask[j:j+input_lengths[i]-predicted_inputs[i],:] = 0
+            self.output_mask[j:j+input_lengths[i]-predicted_inputs[i],:] = float('-inf')
+            j+=input_lengths[i]
         j=0
         for i, mod in enumerate(input_mods):
             # self.output_mask[j:j+input_lengths[i]-predicted_inputs[i],:] = 0
@@ -258,7 +264,7 @@ class TransformerModel(BaseModel):
                     for i,mod in enumerate(self.output_mods):
                         #output_seq[i] = torch.cat([output_seq[i], inputs_[i][t+self.input_time_offsets[i]+self.input_lengths[i]:t+self.input_time_offsets[i]+self.input_lengths[i]+1]+0.15*torch.randn(1,219).cuda()])
                         output_seq[i] = torch.cat([output_seq[i], self.outputs[i][:1].detach().clone()])
-                        #print(self.outputs[i][:1])
+                        # print(self.outputs[i][:1])
                         #print(inputs_[i][t+self.input_time_offsets[i]+self.input_lengths[i]:t+self.input_time_offsets[i]+self.input_lengths[i]+1]+0.15*torch.randn(1,219).cuda())
                     #output_seq = torch.cat([output_seq, x[opt.input_seq_len+t+1:opt.input_seq_len+t+2,:,:219]])
                 if t < sequence_length-1:
@@ -267,8 +273,8 @@ class TransformerModel(BaseModel):
                             j = self.output_mods.index(mod)
                             #input_tmp[i] = torch.cat([input_tmp[i][1:],self.outputs[j][-1:].detach().clone()],0)
                             input_tmp[i] = torch.cat([input_tmp[i][1:-self.predicted_inputs[i]+1],self.outputs[j].detach().clone()],0)
-                            # print(torch.mean((inputs_[i][t+self.input_time_offsets[i]+self.input_lengths[i]-self.predicted_inputs[i]+1:t+self.input_time_offsets[i]+self.input_lengths[i]-self.predicted_inputs[i]+1+1]-self.outputs[j][:1].detach().clone())**2))
-                            #input_tmp[i] = torch.cat([input_tmp[i][1:],inputs_[i][t+self.input_time_offsets[i]+self.input_lengths[i]:t+self.input_time_offsets[i]+self.input_lengths[i]+1]],0)
+                            # input_tmp[i] = torch.cat([input_tmp[i][1:],inputs_[i][t+self.input_time_offsets[i]+self.input_lengths[i]:t+self.input_time_offsets[i]+self.input_lengths[i]+1]],0)
+                            print(torch.mean((inputs_[i][t+self.input_time_offsets[i]+self.input_lengths[i]-self.predicted_inputs[i]+1:t+self.input_time_offsets[i]+self.input_lengths[i]-self.predicted_inputs[i]+1+1]-self.outputs[j][:1].detach().clone())**2))
                             #print(torch.mean((inputs_[i][t+self.input_time_offsets[i]+self.input_lengths[i]-self.predicted_inputs[i]+1:t+self.input_time_offsets[i]+self.input_lengths[i]-self.predicted_inputs[i]+1+self.output_lengths[j]]-self.outputs[j].detach().clone())**2))
                             # input_seq[:,:,i:i+dmod] = torch.cat([input_seq[1:,:,i:i+dmod],x[opt.input_seq_len+t+1:opt.input_seq_len+t+2,:,i:i+dmod]],0)
                             #print(input_tmp[i][t+self.input_time_offsets[i]+self.input_lengths[i]+1:t+self.input_time_offsets[i]+self.input_lengths[i]+1+1])

sandbox.py
@@ -102,19 +102,23 @@ mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1,
 #plotting masks hmm
 #mega hacky lel
 
-mask = np.load("d2d79782-ad83-4617-9211-5f46baf8a3b8.np.npy")
+import numpy as np
+import matplotlib.pyplot as plt
+mask = np.load("9018bc11-db47-4efb-be12-9ecddecc0acd.np.npy")
 mask[0][120:140]
 mask[0][:140]
 import torch
-
 maskt = torch.from_numpy(mask)
 
 ps = torch.nn.functional.softmax(maskt,dim=-1)
 ps[0]
-
-plt.matshow(mask[4])
-plt.matshow(mask[3])
+mask[1].max()
+mask[1].max()
+mask.shape
+mask[2]
+plt.matshow(mask[2])
 plt.matshow(mask[9])
+plt.matshow(mask[5])
 plt.matshow(mask[9][0:1])
 
 
@@ -129,3 +133,19 @@ envelope = librosa.onset.onset_strength(y=y_wav,hop_length=480)
 (y_wav.shape[0]/48000)/0.01
 
 envelope.shape
+
+
+###################
+##
+
+seq_id="gWA_sFM_cAll_d27_mWA2_ch17"
+# seq_id="mambo"
+
+# sf = np.load("data/features/"+seq_id+".mel_ddcpca_scaled.npy")
+sf = np.load("test_data/"+seq_id+".mel_ddcpca_scaled.npy")
+# mf = np.load("data/features/"+seq_id+".joint_angles_scaled.npy")
+mf = np.load("test_data/"+seq_id+".joint_angles_scaled.npy")
+
+sf.shape
+sf
+plt.matshow(sf)

scripts/generation/generate_stage2.py
@@ -21,8 +21,6 @@ import models.constants as constants
 from math import ceil
 from scipy import signal
 
-from scripts.generation.level_generation_utils import extract_features, make_level_from_notes, get_notes_from_stepmania_file
-
 parser = argparse.ArgumentParser(description='Generate Beat Saber level from song')
 parser.add_argument('--song_path', type=str)
 parser.add_argument('--json_file', type=str)
@@ -76,6 +74,7 @@ opt = Struct(**opt)
 
 model = create_model(opt)
 model.setup()
+
 checkpoint = "iter_"+checkpoint
 model.load_networks(checkpoint)
 
@@ -83,13 +82,14 @@ model.load_networks(checkpoint)
 #%%
 
 # seq_id="gLH_sBM_cAll_d16_mLH1_ch04"
-#seq_id="gWA_sBM_cAll_d26_mWA1_ch10"
-seq_id="mambo"
+# seq_id="gWA_sBM_cAll_d26_mWA1_ch10"
+seq_id="gWA_sFM_cAll_d27_mWA2_ch17"
+# seq_id="mambo"
 
-# sf = np.load("data/features/"+seq_id+".mp3_mel_ddcpca.npy")
-sf = np.load("test_data/"+seq_id+".mp3_mel_ddcpca.npy")
-# mf = np.load("data/features/"+seq_id+".pkl_joint_angles_mats.npy")
-mf = np.load("test_data/"+seq_id+".pkl_joint_angles_mats.npy")
+# sf = np.load("data/features/"+seq_id+".mel_ddcpca_scaled.npy")
+sf = np.load("test_data/"+seq_id+".mel_ddcpca_scaled.npy")
+# mf = np.load("data/features/"+seq_id+".joint_angles_scaled.npy")
+mf = np.load("test_data/"+seq_id+".joint_angles_scaled.npy")
 # mf_mean=np.mean(mf,0,keepdims=True)
 # mf_std = np.std(mf,0,keepdims=True)+1e-5
 # sf = (sf-np.mean(sf,0,keepdims=True))/(np.std(sf,0,keepdims=True)+1e-5)
@@ -105,13 +105,13 @@ mf = np.load("test_data/"+seq_id+".pkl_joint_angles_mats.npy")
 # features = features.transpose(1,0)
 # print(features.shape)
 features = {}
-features["in_pkl_joint_angles_mats"] = np.expand_dims(np.expand_dims(mf.transpose(1,0),0),0)
-features["in_mp3_mel_ddcpca"] = np.expand_dims(np.expand_dims(sf.transpose(1,0),0),0)
+features["in_joint_angles_scaled"] = np.expand_dims(np.expand_dims(mf.transpose(1,0),0),0)
+features["in_mel_ddcpca_scaled"] = np.expand_dims(np.expand_dims(sf.transpose(1,0),0),0)
 
-predicted_modes = model.generate(features)[0]
+predicted_modes = model.generate(features)[0].cpu().numpy()
 
 # predicted_modes = (predicted_modes[0].cpu().numpy()*mf_std + mf_mean)
-transform = pickle.load(open("data/features"+'/'+'pkl_joint_angles_mats'+'_'+'scaler'+'.pkl', "rb"))
+transform = pickle.load(open("test_data"+'/'+'pkl_joint_angles_mats'+'_'+'scaler'+'.pkl', "rb"))
 predicted_modes = transform.inverse_transform(predicted_modes)
 
 print(predicted_modes)